4b57589965e5a5adfc65514147244321e09c4687
J_D_L <jocelyndeleon52@gmail.com>
2021-06-25 11:25:49 -0700

    add level 1 updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,3 +1,5 @@
+//Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+
 // Assign the data from data.js to a descriptive variable
 var table = data;
 
@@ -14,10 +16,14 @@ table.forEach(function(ufoSighting) {
 //Print each UFO sighting value
 Object.entries(ufoSighting).forEach(function([key, value]) {
     console.log(key, value);
-    
+
 //Add cell to row for each value
     var cell = row.append("td");
     cell.text(value);
 });
 
 });
+
+
+
+
