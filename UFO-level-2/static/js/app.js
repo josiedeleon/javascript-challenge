0c58a603b285be42b5299c77503f1fad80c3dba5
J_D_L <jocelyndeleon52@gmail.com>
2021-06-25 11:24:56 -0700

    add level2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,62 @@
-// from data.js
-var tableData = data;
+//Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
 
-// YOUR CODE HERE!
+// Assign the data from data.js to a descriptive variable
+var table = data;
+
+// set reference to the table body
+var tableBody = d3.select("tbody");
+
+// Set UFO sighting value for each column
+table.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+
+//Add table row for each UFO sighting
+    var row = tableBody.append("tr");
+
+//Print each UFO sighting value
+Object.entries(ufoSighting).forEach(function([key, value]) {
+    console.log(key, value);
+
+//Add cell to row for each value
+    var cell = row.append("td");
+    cell.text(value);
+});
+
+});
+
+//Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
+
+//Select button to Search
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+
+    tableBody.html("");
+
+//Select datetime to input
+    var inputElement = d3.select("#datetime");
+
+//Get value of input date
+    var inputValue = inputElement.property("value");
+
+ //Print input value
+    console.log(inputValue);
+
+//filter search ufo Sighting by input datetime
+    var filteredSearch = table.filter(sighting => sighting.datetime === inputValue);
+
+    console.log(filteredSearch);
+
+filteredSearch.forEach(function(selections) {
+    console.log(selections);
+
+    var row = tableBody.append("tr");
+
+    Object.entries(selections).forEach(function([key, value]) {
+        console.log(key, value);
+
+        var cell = row.append("td");
+        cell.text(value);
+});
+
+});
+});
